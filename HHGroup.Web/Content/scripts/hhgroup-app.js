5ec40bc25197e99f7ea2ffd1632e4d2b721d93ad
sathish-kn <sathish.kn@outlook.com>
2015-12-11 23:02:20 +0530

    Worked on carousal and menus
    Worked on carousal

--- HHGroup.Web/Content/scripts/hhgroup-app.js
+++ HHGroup.Web/Content/scripts/hhgroup-app.js
@@ -4,11 +4,13 @@ var hhRoot = angular.module('hhgroup', ['ngRoute']);
 // Register the routing for the application
 hhRoot.config(['$routeProvider', '$locationProvider', '$httpProvider',
 function ($routeProvider, $locationProvider, $httpProvider) {
-    $routeProvider.
-        when('/home', { templateUrl: '/Content/views/home.html', controller: 'home-controller' }).
-     otherwise({
-         redirectTo: '/home'
-     });
+    $routeProvider
+        .when('/home', { templateUrl: '/Content/views/home.html', controller: 'home-controller' })
+        .when('/about', { templateUrl: '/Content/views/about.html', controller: 'about-controller' })
+        .when('/contact', { templateUrl: '/Content/views/contact.html', controller: 'contact-controller' })
+        .otherwise({
+             redirectTo: '/home'
+        });
 
 }]);
 
